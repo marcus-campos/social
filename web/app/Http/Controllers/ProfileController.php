6eeffccb345b4eb255f11d0cbff0b2ef80905b6d
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-05-17 15:19:40 -0300

    Controllers and crud methods

---
+++ web/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function  update()
+    {
+
+    }
+
+    public function destory()
+    {
+
+    }
+}
